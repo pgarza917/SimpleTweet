110408402564754c55dd3aa5d5b8d3d985693c10
Pablo Garza <pgarza917@fb.com>
2020-06-30 16:52:37 -0400

    Added pull to refresh functionality for timeline

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -54,6 +54,18 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         return tweets.size();
     }
 
+    // Clear all items in the Recycler View
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items
+    public void addAll(List<Tweet> tweetList) {
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
     // Define a View Holder (starting point)
     public class ViewHolder extends RecyclerView.ViewHolder {
 
