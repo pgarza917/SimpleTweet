cdfb453f96189fcbc8983f45a37a3d8640148568
Pablo Garza <pgarza917@fb.com>
2020-07-02 19:38:37 -0400

    Added rounded corners and center cropping on embedded photo media

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -14,7 +14,10 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
 import org.w3c.dom.Text;
@@ -158,8 +161,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
                 ivMedia.setVisibility(View.VISIBLE);
                 Glide.with(context)
                         .load(tweet.mediaUrl)
-                        .transform(new RoundedCornersTransformation(30, 10))
-                        .centerCrop()
+                        .transform(new MultiTransformation(new CenterCrop(), new RoundedCornersTransformation(10, 0)))
                         .into(ivMedia);
             }
             else {
